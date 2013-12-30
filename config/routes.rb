f01b408fc46698751ad4e9180fd8c9eddd271d07
Mark Peace <mark@learnsomestuff.com>
2013-12-30 18:19:17 +0000

    created a temporary splash page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 RailsBoilerplate::Application.routes.draw do
+  root "splash#index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
